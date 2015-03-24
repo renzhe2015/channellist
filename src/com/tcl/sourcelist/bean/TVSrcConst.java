3cbcf6ffaef78b773864be5225e91294a1971737
renzhe2015 <747651446@qq.com>
2015-03-24 15:13:56 +0800

    change source

--- src/com/tcl/sourcelist/bean/TVSrcConst.java
+++ src/com/tcl/sourcelist/bean/TVSrcConst.java
@@ -8,7 +8,6 @@ public class TVSrcConst
 	public static final int SOURCMONI      = TvManager.SOURCE_ATV1;
 	public static final int SOURCE_AV1     = TvManager.SOURCE_AV1;
 	public static final int SOURCE_AV2     = TvManager.SOURCE_AV2;
-	public static final int SOURCE_AV3     = TvManager.SOURCE_AV3;
 	public static final int SOURCE_VGA     = TvManager.SOURCE_VGA1;
 	public static final int SOURCE_HDMI1   = TvManager.SOURCE_HDMI1;// jinshujing
 	public static final int SOURCE_HDMI2   = TvManager.SOURCE_HDMI2;//
