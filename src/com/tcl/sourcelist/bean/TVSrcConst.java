69ae1da69f040c1d9d56adc125ad135867a0e5a4
renzhe2015 <747651446@qq.com>
2015-03-24 15:48:32 +0800

    delete HDMI3

--- src/com/tcl/sourcelist/bean/TVSrcConst.java
+++ src/com/tcl/sourcelist/bean/TVSrcConst.java
@@ -11,6 +11,5 @@ public class TVSrcConst
 	public static final int SOURCE_VGA     = TvManager.SOURCE_VGA1;
 	public static final int SOURCE_HDMI1   = TvManager.SOURCE_HDMI1;// jinshujing
 	public static final int SOURCE_HDMI2   = TvManager.SOURCE_HDMI2;//
-	public static final int SOURCE_HDMI3   = TvManager.SOURCE_HDMI3;//
 	public static final int SOURCE_USB   =TvManager.SOURCE_PLAYBACK ;//
 }
